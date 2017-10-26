2e5ca6ec26e2ed6d64445da915e41a9dd8afc731
Adrian Zhang <adrianzn@163.com>
2017-10-26 18:41:30 +0800

    Update uninstall_3_app.py

--- uninstall_3_app.py
+++ uninstall_3_app.py
@@ -1,38 +1,44 @@
 # Author: AdrianZhang
 # Coding Time: 2017/10/25
-# Script function: 一个实现“卸载多个Android手机中所有第三方包的脚本”。
+# Script function: 一个实现“卸载当前已连接的Android手机中所有第三方包”的脚本。
 
 import os
 
-def get_devices_name():
-    device_list = os.popen("adb devices").read().split("\n")
-    devices = []
-    for device in device_list:
-        if "\t" in device:
-            devices.append(device.split("\t")[0])
-    return devices
+class Uninstall3App(object):
+    def __init__(self):
+        self.device_name = ""
+        self.package_name = []
 
-def get_3_app_packages_name():
-    package_list = os.popen("adb shell pm list packages -3").readlines()
-    package_name = []
-    for package in package_list:
-        package_name.append(package.split(":")[-1].splitlines()[0])
-    return package_name
+    def get_devices_name(self):
+        try:
+            device_list = os.popen("adb devices").read().split("\n")
+            for device in device_list:
+                if "\t" in device:
+                    self.device_name = device.split("\t")[0]
+        except Exception as msg:
+            print(msg)
 
-def uninstall_3_app():
-    device_name = get_devices_name()
-    package_name = get_3_app_packages_name()
-    for device in device_name:
-        for package in package_name:
+    def get_3_app_packages_name(self):
+        try:
+            package_list = os.popen("adb -s {} shell pm list packages -3".format(self.device_name)).readlines()
+            for package in package_list:
+                self.package_name.append(package.split(":")[-1].replace("\n", ""))
+        except Exception as msg:
+            print(msg)
+
+    def uninstall_3_app(self):
+        for package in self.package_name:
             try:
-                content = os.popen("adb -s {} shell pm uninstall {}".format(device, package)).read().replace("\n", "")
+                log_info = os.popen("adb -s {} shell pm uninstall {}".format(self.device_name, package)).read()
+                content = log_info.replace("\n", "")
                 if "Success" in content:
-                    print("{}: {} uninstall {}!".format(device, package, content))
-                else:
-                    print("{}: {} uninstall Fail! Details as Follows: {}".format(device, package, content))
+                    print("{}: {} uninstall {}!".format(self.device_name, package, content))
             except Exception as msg:
                 print(msg)
 
 if __name__ == '__main__':
-    uninstall_3_app()
+    do_test = Uninstall3App()
+    do_test.get_devices_name()
+    do_test.get_3_app_packages_name()
+    do_test.uninstall_3_app()
 
