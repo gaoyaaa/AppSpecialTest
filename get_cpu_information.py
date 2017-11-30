310baa47fd15cc185e20b84370087a00623a039e
Adrian Zhang <adrianzn@163.com>
2017-11-30 20:48:56 +0800

    Update get_cpu_information.py

--- get_cpu_information.py
+++ get_cpu_information.py
@@ -4,6 +4,7 @@
 
 import os
 import csv
+import matplotlib.pyplot as plt
 
 def connect_device():
     try:
@@ -16,43 +17,25 @@ def connect_device():
     except Exception as e:
         print(e)
 
-def get_once_cpu_data():
-    cpu_data_list = []
+def get_once_top_data():
+    top_data_list = []
     try:
-        cpu_data = os.popen('adb shell "top -n 1 -d 1 | grep com.snda.wifilocating"').read()
-        split_cpu_data = cpu_data.strip().split('\n\n')
-        for each in split_cpu_data:
+        top_data = os.popen('adb shell "top -n 1 -d 1 | grep com.snda.wifilocating"').read()
+        split_top_data = top_data.strip().split('\n\n')
+        for each in split_top_data:
             split_each = each.split()
             clean_each = ",".join(split_each)
-            cpu_data_list.append(clean_each.split(','))
-        return cpu_data_list
+            top_data_list.append(clean_each.split(','))
+        return top_data_list
     except Exception as e:
         print(e)
 
-def format_data(cpu_data_list):
-    """
-    PID: 应用程序ID
-    S: 进程状态(S:休眠; R:正在运行; Z:僵死状态; N:进程优先值为负数)
-    #THR: 程序所用线程数
-    VSS: 虚拟耗用内存
-    RSS: 实际使用物理内存
-    UID: 用户身份ID
-    Name: 应用程序名称
-    """
+def format_data(top_data_list):
     formatted_data = [
-        ["PID",
-         "PR",
-         "CPU%",
-         "S",
-         "#THR",
-         "VSS",
-         "RSS",
-         "PCY",
-         "UID",
-         "Name"],
+        ["PID", "PR", "CPU%", "S", "#THR", "VSS", "RSS", "PCY", "UID", "Name"],
     ]
 
-    for each in cpu_data_list:
+    for each in top_data_list:
         formatted_data.append(each)
     return formatted_data
 
@@ -63,9 +46,39 @@ def write_csv(formatted_data):
         writer.writerow(unit)
     file.close()
 
+def get_vss():
+    pass
+
+def get_rss():
+    pass
+
+def visualization(vss_list, rss_list):
+    list_x1 = [1, 5, 7, 9, 13, 16]
+    list_y1 = [15, 50, 80, 40, 70, 50]
+    plt.plot(list_x1, list_y1, color="blue", linestyle="-", label="Python")
+
+    list_x2 = [2, 6, 8, 11, 14, 16]
+    list_y2 = [10, 40, 30, 50, 80, 60]
+    plt.plot(list_x2, list_y2, color="red", linestyle="--", label="Java")
+
+    list_x3 = [3, 7, 9, 12, 15, 17]
+    list_y3 = [12, 45, 35, 55, 85, 65]
+    plt.plot(list_x3, list_y3, color="yellow", linestyle=":", label="Swift")
+
+    plt.legend()
+    plt.xlim(0, 20)
+    plt.ylim(0, 100)
+    plt.title("People's Like")
+    plt.xlabel("Number")
+    plt.ylabel("Times")
+    plt.show()
+
 if __name__ == '__main__':
     log = connect_device()
     cpu_data_list = get_once_cpu_data()
     format_data = format_data(cpu_data_list)
     write_csv(format_data)
+
+    play = visualization()
+    print(play)
 
